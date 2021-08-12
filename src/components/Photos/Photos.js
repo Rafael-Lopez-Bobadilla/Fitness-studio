9d0e1257b4fba39a28b3f4d49925d80b0931d496
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2021-08-12 17:01:38 -0500

    ready to start photos component

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -5,14 +5,7 @@ export default function Photos() {
   return (
     <div className='photos'>
       <SectionTitle title='PHOTOS' />
-      <div className='photos__container'>
-        <div className='photos__carousel'>
-          {photos.map((photo, index) => {
-            return <img src={photo} className='photos__small' key={`${index}`} alt='carousel' />
-          })}
-        </div>
-        <img src={photos[0]} className='photos__bigPhoto' alt='big' />
-      </div>
+
     </div>
   )
 }
