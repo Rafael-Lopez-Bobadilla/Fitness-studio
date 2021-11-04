9e0fd4d2c0c0a808883de5bb9b4797a88abda705
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2021-11-04 17:58:26 -0600

    reponsive design changes

--- src/components/Header/scrollTo.js
+++ src/components/Header/scrollTo.js
@@ -1,6 +1,7 @@
 export const scrollTo = (elementClassName) => {
   const y = document.getElementsByClassName(elementClassName.toLowerCase())[0]
     .getBoundingClientRect().top + window.pageYOffset
+  //--navbarHeight variable(defined on index.css) equals 50px
   window.scrollTo({
     top: y - 50,
     behavior: "smooth"
